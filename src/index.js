dbe8b3da83aa0ed520fd0bfd04023e0c2de9bc60
Emile-Webdev <emile.webdev@hotmail.com>
2024-08-13 12:57:54 +0200

    update 7
    responsiveness

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import {
     LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY,
     save
 } from './localStorage';
+import {
+    getRandomHexColor,
+    convertHexToRGBA
+} from './colors';
 
 // --- GLOBAL VARIABLES PROJECTS ---
 const newProjectForm = document.querySelector('[data-new-project-form]');
@@ -232,31 +236,10 @@ function renderTasks() {
     })
 }
 
-// --- HELPER FUNCTIONS ---
 function clearChildElements(element) {
     while(element.firstChild) {
         element.removeChild(element.firstChild);
     }
-}
-
-function getRandomHexColor() {
-    var hex = (Math.round(Math.random() * 0xffffff)).toString(16);
-    while(hex.length < 6) hex = "0" + hex;
-    return `#${hex}`;
-}
-
-function convertHexToRGBA(hexCode, opacity) {
-    let hex = hexCode.replace('#', '');
-
-    if(hex.length === 3) {
-        hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`;
-    }
-
-    const r = parseInt(hex.substring(0, 2), 16);
-    const g = parseInt(hex.substring(2, 4), 16);
-    const b = parseInt(hex.substring(4, 6), 16);
-
-    return `rgba(${r},${g},${b},${opacity / 100})`;
 }
 
 window.addEventListener('load', render);
